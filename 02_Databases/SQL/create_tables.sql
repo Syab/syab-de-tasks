284df2a0829fedc3e28e161cc665412a4355391b
syab <syabster.phang18@gmail.com>
2022-04-02 16:29:36 +0800

    task 2 - database modify load data

--- 02_Databases/SQL/create_tables.sql
+++ 02_Databases/SQL/create_tables.sql
@@ -1,23 +1,23 @@
 -- Create Car Manufacturer
-CREATE TABLE IF NOT EXISTS CAR_MANUFACTURER (
+CREATE TABLE IF NOT EXISTS CAR_MANUFACTURERS (
   manufacturer_id INT NOT NULL,
   manufacturer_name VARCHAR(250) NOT NULL,
   PRIMARY KEY (manufacturer_id)
 );
 
 -- Create Car Model
-CREATE TABLE IF NOT EXISTS CAR_MODEL (
+CREATE TABLE IF NOT EXISTS CAR_MODELS (
     model_id INT NOT NULL,
     model_name VARCHAR(250) NOT NULL,
     manufacturer_id INT NOT NULL,
     car_weight FLOAT NOT NULL,
     price FLOAT NOT NULL,
     PRIMARY KEY (model_id),
-    CONSTRAINT fk_manufacturer FOREIGN KEY(manufacturer_id) REFERENCES CAR_MANUFACTURER(manufacturer_id)
+    CONSTRAINT fk_manufacturers FOREIGN KEY(manufacturer_id) REFERENCES CAR_MANUFACTURERS(manufacturer_id)
 );
 
 -- Create Customer
-CREATE TABLE IF NOT EXISTS CUSTOMER (
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
   customer_id INT NOT NULL,
   customer_name VARCHAR(250) NOT NULL,
   customer_phone_number INT NOT NULL,
@@ -25,31 +25,31 @@ CREATE TABLE IF NOT EXISTS CUSTOMER (
 );
 
 -- Create Salesperson
-CREATE TABLE IF NOT EXISTS SALESPERSON (
+CREATE TABLE IF NOT EXISTS SALESPERSONS (
   salesperson_id INT NOT NULL,
   salesperson_name VARCHAR(250) NOT NULL,
   PRIMARY KEY (salesperson_id)
 );
 
 -- Create Car
-CREATE TABLE IF NOT EXISTS CAR (
-    car_id INT NOT NULL,
-    model_id INT NOT NULL,
-    manufacturer_id INT NOT NULL,
-    PRIMARY KEY (car_id),
-    CONSTRAINT fk_model FOREIGN KEY(model_id) REFERENCES CAR_MODEL(model_id),
-    CONSTRAINT fk_manufacturer FOREIGN KEY(manufacturer_id) REFERENCES CAR_MANUFACTURER(manufacturer_id)
-);
+-- CREATE TABLE IF NOT EXISTS CAR (
+--     car_id INT NOT NULL,
+--     model_id INT NOT NULL,
+--     manufacturer_id INT NOT NULL,
+--     PRIMARY KEY (car_id),
+--     CONSTRAINT fk_model FOREIGN KEY(model_id) REFERENCES CAR_MODEL(model_id),
+--     CONSTRAINT fk_manufacturer FOREIGN KEY(manufacturer_id) REFERENCES CAR_MANUFACTURER(manufacturer_id)
+-- );
 
 -- Create Car Transaction
-CREATE TABLE IF NOT EXISTS CAR_TRANSACTION (
-   transaction_id INT NOT NULL,
-   serial_no INT NOT NULL,
-   car_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS CAR_TRANSACTIONS (
+   transaction_id BIGINT NOT NULL,
    customer_id INT NOT NULL,
    salesperson_id INT NOT NULL,
+   model_id INT NOT NULL,
+   serial_no VARCHAR(250) NOT NULL,
    PRIMARY KEY (transaction_id),
-   CONSTRAINT fk_car FOREIGN KEY(car_id) REFERENCES CAR(car_id),
-   CONSTRAINT fk_customer FOREIGN KEY(customer_id) REFERENCES CUSTOMER(customer_id),
-   CONSTRAINT fk_salesperson FOREIGN KEY(salesperson_id) REFERENCES SALESPERSON(salesperson_id)
+   CONSTRAINT fk_models FOREIGN KEY(model_id) REFERENCES CAR_MODELS(model_id),
+   CONSTRAINT fk_customers FOREIGN KEY(customer_id) REFERENCES CUSTOMERS(customer_id),
+   CONSTRAINT fk_salespersons FOREIGN KEY(salesperson_id) REFERENCES SALESPERSONS(salesperson_id)
 );
--- 02_Databases/solution.py
+++ 02_Databases/solution.py
@@ -1,8 +1,38 @@
+#!/usr/bin/python
 import psycopg2
+import os
 
+# Dictionary of DBs and mock data file source
+db_files = {
+    'car_manufacturer' : './DATA/manufacturers.csv',
+    'car_model' : './DATA/models.csv',
+    'salesperson' : './DATA/salespersons.csv',
+    'customer' : './DATA/customers.csv',
+    'car_transaction' : './DATA/transactions.csv',
+}
 
+# Connect to DB
 conn = psycopg2.connect(
-    host="localhost",
-    database="postgres",
-    user="postgres",
-    password="somesafepassword")
+   database="postgres", 
+   user='postgres', 
+   password='postgrespw', 
+   host='127.0.0.1', 
+   port= '5432'
+)
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+for i in db_files:
+    print(i, db_files[i])
+    with open(db_files[i], 'r') as f :
+        next(f)
+        cursor.copy_from(f, i, sep=',')
+
+
+#Fetching 1st row from the table
+# result = cursor.fetchall();
+# print(result)
+
+#Closing the connection
+conn.close()
