1c8f240de1624dd5a4d3e2fb045d09b8ed8fcef4
syab <syabster.phang18@gmail.com>
2022-04-03 15:49:18 +0800

    task 1 complete

--- 01_Data_Pipelines/processfile.py
+++ 01_Data_Pipelines/processfile.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import numpy as np
 import os
-from datetime import date
+from datetime import date, datetime
 import sys
  
 filename = sys.argv[1]
-print(filename)
+# print(filename)
 
-today = date.today()
-outputpath = './OUTPUT/'+str(today)+'_output.csv'
+now = datetime.now()
+dt_string = now.strftime("%Y-%m-%d_%H%M%S")
+
+outputpath = './output/'+str(dt_string)+'_output.csv'
 
 data = filename
 df= pd.read_csv(data)
-
-
-# Ensure all values are float not strings 
-df['price'] = df['price'].astype(float)
-# Get values above 100
-df['above_100'] = df['price'].apply(lambda x: 'true' if x > 100 else 'false')
 
 # clean names
 df['name'].replace('(\,|\.|Miss|Ms.|Mrs.|Mrs. |Jr.|Jr. |Dr.|Dr. |Mr.|Mr. |,Mr.|MD|DDS|PhD|DVM|II|III|IV)','',regex=True, inplace=True)
@@ -25,10 +21,17 @@ df['name'] = df['name'].str.strip()
 df['first_name'] = df['name'].str.split(" ").str[0]
 df['last_name'] = df['name'].str.split(" ").str[-1]
 
-df.dropna(subset= ['name','first_name','last_name'],inplace =True)
+df.dropna(subset= ['name','first_name','last_name', 'price'],inplace =True)
+
+# Ensure all values are float not strings 
+df['price'] = df['price'].astype(float)
+# Get values above 100
+df['above_100'] = df['price'].apply(lambda x: 'true' if x > 100 else 'false')
 
 df = df.drop(['name'],axis =1)
 
 # reindex for presentation 
 df = df[['first_name','last_name','price','above_100']]
 df.to_csv(outputpath,index = False)
+
+print("OK")
--- 01_Data_Pipelines/processfile.sh
+++ 01_Data_Pipelines/processfile.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 mkdir -p OUTPUT
-python processfile.py > "./output/file.txt"
+mkdir -p processed
+chmod -R 766 processed
+
+fileList=$(ls DATA/*.csv)
+
+for file in ${fileList}; do
+    echo $file
+    python processfile.py $file > dump.txt
+    dumpContent=$(cat dump.txt)
+    
+    if [[ ${dumpContent} == "OK" ]]
+    then
+        echo "File processing OK"
+        mv ${file} processed/
+    else
+        mv ${file} unprocessed/
+    fi 
+    rm -f dump.txt
+done
